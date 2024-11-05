9f95b6cbdc358f5ba514def6b6601e8386cc1112
aubrey-baker <AubreyBakerEmail@gmail.com>
2024-11-05 12:23:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post('/upload-audio', upload.single('audioFile'), (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 })
